dc42e1db8c5d3519c02db37ba2b8e926da60b632
Matt Elliott <melliott@ccistudios.com>
2012-08-02 12:43:51 -0400

    white space removed

--- template/templates/cyrheault/index.php
+++ template/templates/cyrheault/index.php
@@ -11,35 +11,35 @@ if ($menu && $menu->getActive())
     $menu = $menu->getActive()->alias;
 else
 	$menu = "";
-
+ 
 if ($_SERVER['SERVER_PORT'] === 8888 ||
 		$_SERVER['SERVER_ADDR'] === '127.0.0.1' ||
 		stripos($_SERVER['SERVER_NAME'], 'ccistaging') !== false ||
 		stripos($_SERVER['SERVER_NAME'], 'dev') === 0 ||
 		stripos($_SERVER['SERVER_NAME'], 'xip.io') !== false) {
-
+ 
 	$testing = true;
 	error_reporting(E_ALL);
 	ini_set('display_errors', '1');
 } else {
 	$testing = false;
 }
-
+ 
 JHTML::_('behavior.mootools');
 $analytics = "UA-33325927-1";
 $typekit = 'olk3sbb';
 ?>
-
+ 
 <head>
 	<meta charset="utf-8" />
 	<?= ($testing)? '':  '<meta http-equiv="X-UA-Compatible" contents="IE=edge,chrome=1">' ?>
-
+ 
  	<jdoc:include type="head" />
-
+ 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 	<link rel="icon" type="image/x-icon" href="/templates/<?= $this->template ?>/resources/favicon.ico">
 	<link rel="apple-touch-icon" href="/templates/<?= $this->template ?>/resources/apple-touch-icon.png">
-
+ 
 	<!-- load css -->
 	<?php if ($testing): ?>
 		<!--[if (gt IE 8) | (IEMobile)]><!-->
@@ -56,7 +56,7 @@ $typekit = 'olk3sbb';
 			<link rel="stylesheet" href="/templates/<?= $this->template ?>/css/template-ie.min.css">
 		<![endif]-->
 	<?php endif; ?>
-
+ 
 	<script src="/templates/<?= $this->template ?>/js/libs/modernizr-1.7.min.js"></script>
 	<?php if ($typekit): ?>
 		<!-- load typekit -->
@@ -64,14 +64,14 @@ $typekit = 'olk3sbb';
 		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 	<?php endif; ?>
 </head>
-
+ 
 <body class="<?= $menu ?>">
-
+ 
 	<div id="mobile-menu">
 		<jdoc:include type="modules" name="mobile-menu" style="xhtml" />
 		<div class="clear"></div>
 	</div>
-
+ 
 	<div id="wrapper">
 		
 			<div id="header"><div class="container">
@@ -86,7 +86,7 @@ $typekit = 'olk3sbb';
 			</div>
 		
 			<div class="clear"></div>
-
+ 
 			<div id="main">
 				<div id="top">
 					<jdoc:include type="modules" name="top" style="rounded" />
@@ -112,7 +112,7 @@ $typekit = 'olk3sbb';
 				</div>
 			</div>
 		</div>
-
+ 
 		<div id="footer"><div class="container">
 			<div id="copyright">
 				&copy; <?php echo date('Y') ?> cy rheault construction Ltd. All Rights Reserved.<br />
@@ -123,11 +123,11 @@ $typekit = 'olk3sbb';
 		</div></div>
 		
 	</div>
-
+ 
 	<div class="hidden">
 		<jdoc:include type="modules" name="hidden" style="raw" />
 	</div>
-
+ 
 	<!-- load scripts -->
 	<?php if ($testing): ?>
 		<script src="/templates/<?= $this->template ?>/js/columns.js"></script>
