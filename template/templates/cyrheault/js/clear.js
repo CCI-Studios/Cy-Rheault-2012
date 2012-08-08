d6d94ff8ddfcfb01b15536df1ec40c28b870419b
Matt Elliott <melliott@ccistudios.com>
2012-08-08 14:14:35 -0400

    used clearfix class instead of clear

--- template/templates/cyrheault/js/clear.js
+++ template/templates/cyrheault/js/clear.js
@@ -1,4 +1,4 @@
 window.addEvent('domready', function(){
 	var elements = $$('.ie7 .gallery-block:nth-child(3n+1), .ie8 .gallery-block:nth-child(3n+1)');
-	elements.addClass('clear');	
+	elements.addClass('clearfix');	
 });
