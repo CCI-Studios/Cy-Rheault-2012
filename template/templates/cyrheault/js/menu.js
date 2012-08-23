ec5e49d67256fdb365f0a836c1bf57a98f87b200
Jonathan Bennett <jbennett@ccistudios.com>
2012-08-22 22:16:43 -0400

    Updated js scripts

--- template/templates/cyrheault/js/menu.js
+++ template/templates/cyrheault/js/menu.js
@@ -1,17 +1,16 @@
 window.addEvent('domready', function() {
 	var displayers = $$('.show-menu'),
-		wrapper = $('wrapper'),
 		menu = $$('.vmenu.mobile')[0],
 		resize = function() {
 			menu.setStyle('max-height', menu.getElement('ul').getSize().y);
 		};
-		
-		
+
+
 	resize();
 	window.addEvent('resize', function(){
 		resize();
 	});
-		
+
 	displayers.addEvent('click', function(event) {
 		event = new Event(event);
 		event.stop();
--- template/templates/cyrheault/js/scripts.js
+++ template/templates/cyrheault/js/scripts.js
@@ -1,10 +1,24 @@
 /**
- * @depends columns.js
- * @depends dropmenu.js
- * @depends lettering.js
- * @depends menu.js
- * @depends clear.js
- * @depends detectmobilebrowser.js
- * @depends html5.js
- * @depends script-init.js
+ * @codekit-prepend "columns.js"
+ * @codekit-prepend "dropmenu.js"
+ * @codekit-prepend "html5.js"
+ * @codekit-prepend "lettering.js"
+ * @codekit-prepend "menu.js"
+ * @codekit-prepend "clear.js"
+ * @codekit-prepend "detectmobilebrowser.js"
+ * @codekit-prepend "rollover.js"
  */
+
+window.addEvent('domready', function() {
+	// image roll overs
+	new CCI.Rollover('img.rollover');
+	// new CCI.Rollover('li.item-122 img, li.item-105 img', { normal_text: '.', over_text: '_rollover.'});
+
+	// image lettering
+	$$('h1').lettering('words');
+});
+
+window.addEvent('load', function () {
+	// columns
+	// new CCI.Columns($('bottom'), '.module > div > div');
+});
